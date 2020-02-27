cbac0f0b5e01a4d86987acc6eedc8af052a6f459
rashidhatami <39828914+rashidhatami@users.noreply.github.com>
2020-02-27 13:54:47 +0330

    remove bst.java file

--- bst.java
+++
@@ -1,9 +0,0 @@
-
-public class BinarySearchTree extends AbstractBinarySearchTree {
-
-    @Override
-    protected Node createNode(int value, Node parent, Node left, Node right) {
-        return new Node(value, parent, left, right);
-    }
-
-}
